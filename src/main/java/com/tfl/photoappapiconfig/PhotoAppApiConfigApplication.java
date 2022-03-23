ba6ab6c27a68986c4ec81858fa53fac8001874a7
Victor Yamchinov <kodiers@gmail.com>
2022-03-23 23:57:52 +0300

    Initial commit

---
+++ src/main/java/com/tfl/photoappapiconfig/PhotoAppApiConfigApplication.java
@@ -0,0 +1,13 @@
+package com.tfl.photoappapiconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppApiConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoAppApiConfigApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tfl/photoappapiconfig/PhotoAppApiConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfl.photoappapiconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
